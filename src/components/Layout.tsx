99ee2bef31597bd4ecc9d5b3857f72a9131597ae
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-30 07:39:20 +0100

    feat: Render ScrollToTop button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = () => {
     return (
         <>
             <Navbar />
             <Outlet />
+            <ScrollToTop />
             <Footer />
         </>
     )
