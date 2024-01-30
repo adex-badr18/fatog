e7c2ded661e70726a4c4e9de0c87b075253d256c
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-30 19:30:39 +0100

    fix: Resolve modal responsiveness

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
     return (
         <div className={`flex justify-center items-center fixed inset-0 z-50 overflow-auto bg-gray-800 bg-opacity-60 ${isOpen ? 'block' : 'hidden'}`}>
-            <div className="relative max-w-lg p-10 bg-white shadow-md rounded-md">
+            <div className="relative w-full mx-4 sm:mx-0 sm:max-w-lg p-10 bg-white shadow-md rounded-md">
                 {children}
                 <button className="absolute top-0 right-0 p-4 text-xl text-[#0e204d]" onClick={onClose}>
                     <IoCloseSharp />
