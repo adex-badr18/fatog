94103eab845997af8ed403333059baa87e6fc20b
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-03-27 23:18:06 +0100

    feat: Create routes for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,19 @@ import {
     RouterProvider,
     Route
 } from 'react-router-dom';
+import Login from "./pages/Login";
+import SignUp from './pages/SignUp';
+import ProductList from './pages/ProductList';
+import ForgotPassword from './pages/ForgotPassword';
 
 const router = createBrowserRouter(createRoutesFromChildren(
     <Route path='/' element={<Layout />}>
         <Route index element={<Landing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='forgot-password' element={<ForgotPassword />} />
+
+        <Route path='products' element={<ProductList />} />
 
         {/* Catch all */}
         <Route path='*' element={<NotFound />} />
